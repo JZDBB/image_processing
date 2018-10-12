e12395066aa8c7409b7bc49e7fa06e7a20536ec3
XuYining <oxuyining@gmail.com>
2018-10-12 19:23:57 +0800

    orignal

---
+++ Image_Processing/Image_ProcessingView.cpp
@@ -0,0 +1,273 @@
+
+// Image_ProcessingView.cpp : CImage_ProcessingView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "Image_Processing.h"
+#endif
+
+#include "Image_ProcessingDoc.h"
+#include "Image_ProcessingView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CImage_ProcessingView
+
+IMPLEMENT_DYNCREATE(CImage_ProcessingView, CScrollView)
+
+BEGIN_MESSAGE_MAP(CImage_ProcessingView, CScrollView)
+	// 标准打印命令
+	ON_COMMAND(ID_FILE_PRINT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CImage_ProcessingView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_COMMAND(ID_FILE_OPEN, &CImage_ProcessingView::OnFileOpen)
+	ON_COMMAND(IDM_DRAWLINE, &CImage_ProcessingView::OnDrawline)
+	ON_COMMAND(ID_FILE_SAVE_AS, &CImage_ProcessingView::OnFileSaveAs)
+	ON_COMMAND(IDM_SHOWRED, &CImage_ProcessingView::OnShowred)
+//	ON_COMMAND(ID_VIEW_TOOLBAR, &CImage_ProcessingView::OnViewToolbar)
+END_MESSAGE_MAP()
+
+// CImage_ProcessingView 构造/析构
+
+CImage_ProcessingView::CImage_ProcessingView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CImage_ProcessingView::~CImage_ProcessingView()
+{
+}
+
+BOOL CImage_ProcessingView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CScrollView::PreCreateWindow(cs);
+}
+
+// CImage_ProcessingView 绘制
+
+void CImage_ProcessingView::OnDraw(CDC* pDC)
+{
+	CImage_ProcessingDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+	if (!m_Image.IsNull())
+	{
+		m_Image.Draw(pDC->m_hDC,0,0); 
+	}
+	return;
+}
+
+void CImage_ProcessingView::OnInitialUpdate()
+{
+	CScrollView::OnInitialUpdate();
+
+	CSize sizeTotal;
+	// TODO: 计算此视图的合计大小
+	sizeTotal.cx = sizeTotal.cy = 100;
+	SetScrollSizes(MM_TEXT, sizeTotal);
+}
+
+
+// CImage_ProcessingView 打印
+
+
+void CImage_ProcessingView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CImage_ProcessingView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 默认准备
+	return DoPreparePrinting(pInfo);
+}
+
+void CImage_ProcessingView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加额外的打印前进行的初始化过程
+}
+
+void CImage_ProcessingView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 添加打印后进行的清理过程
+}
+
+void CImage_ProcessingView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CImage_ProcessingView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CImage_ProcessingView 诊断
+
+#ifdef _DEBUG
+void CImage_ProcessingView::AssertValid() const
+{
+	CScrollView::AssertValid();
+}
+
+void CImage_ProcessingView::Dump(CDumpContext& dc) const
+{
+	CScrollView::Dump(dc);
+}
+
+CImage_ProcessingDoc* CImage_ProcessingView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CImage_ProcessingDoc)));
+	return (CImage_ProcessingDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CImage_ProcessingView 消息处理程序
+
+
+void CImage_ProcessingView::OnFileOpen()
+{
+	// TODO: 在此添加命令处理程序代码
+	CFileDialog dlg(TRUE);    //为打开文件创建一个变量
+	if(IDOK == dlg.DoModal())    //调用函数打开一个对话框，并判断是否打开成功
+	{
+		if(!m_Image.IsNull()) m_Image.Destroy();//判断是否已经有图片，有的话进行清除
+		m_Image.Load(dlg.GetPathName());//获得图片的地址，并且加载图片
+
+		//获得图片的大小，并按其大小设置滚动条的初始窗口大小等参数
+		CSize sizeTotal;
+		int w=m_Image.GetWidth();
+		int h=m_Image.GetHeight();
+		sizeTotal.cx =w;
+		sizeTotal.cy =h;
+		SetScrollSizes(MM_TEXT, sizeTotal);
+
+		//Window_Image_w=m_Image.GetWidth();//获得图片的初始大小，为放大缩小功能做准备
+		//Window_Image_h=m_Image.GetHeight();//
+
+
+		Invalidate(1); //强制调用ONDRAW函数
+	}
+}
+
+
+void CImage_ProcessingView::OnDrawline()
+{
+	// TODO: 在此添加命令处理程序代码
+	if(m_Image.IsNull()) return;
+	
+	int w=m_Image.GetWidth();
+	int h=m_Image.GetHeight();
+
+	for (int k=0;k<w;k++)
+	{
+		m_Image.m_pBits[0][10][k]=0;//B
+		m_Image.m_pBits[1][10][k]=0;//G
+		m_Image.m_pBits[2][10][k]=0;//R
+
+
+	}
+
+	Invalidate(1); //强制调用ONDRAW函数
+
+
+
+}
+
+
+void CImage_ProcessingView::OnFileSaveAs()
+{
+	// TODO: 在此添加命令处理程序代码
+	if (m_Image.IsNull()) {
+		MessageBox(_T("你还没有打开一个要保存的图像文件！"));
+		return;
+	}
+	CString strFilter;
+	//strFilter ="位图文件|*.bmp|JPEG 图像文件|*.jpg|GIF 图像文件|*.gif|PNG 图像文件|*.png||";   //
+	strFilter ="所有文件|*.*||";   //
+
+	CFileDialog dlg(FALSE,NULL,NULL,NULL,strFilter);
+	//CFileDialog dlg(FALSE,NULL,NULL,NULL);
+
+	if ( IDOK != dlg.DoModal()) 
+		return;
+	// 如果用户没有指定文件扩展名，则为其添加一个
+	CString strFileName;
+	//CString strExtension;
+	strFileName = dlg.m_ofn.lpstrFile;   //  获得文件名
+	//if(dlg.m_ofn.nFileExtension == 0)     //获得文件扩展名
+	//{
+	//	switch (dlg.m_ofn.nFilterIndex)
+	//	{
+	//	case 1:
+	//		strExtension = "bmp"; break;
+	//	case 2:
+	//		strExtension = "jpg"; break;
+	//	case 3:
+	//		strExtension = "gif"; break;
+	//	case 4:
+	//		strExtension = "png"; break;
+	//	default:
+	//		break;
+	//	}
+	//	strFileName = strFileName + _T(".") + strExtension;
+	//	//strFileName="C:\\Users\\Lenovo\\Desktop\\QSanguosha-Qingming\\abc.bmp";
+	//}
+	// 图像保存
+	HRESULT hResult = m_Image.Save(strFileName);
+	if (FAILED(hResult))
+	{
+		MessageBox(_T("保存图像文件失败！"));
+	}
+
+}
+
+
+void CImage_ProcessingView::OnShowred()
+{
+	// TODO: 在此添加命令处理程序代码
+	if(m_Image.IsNull()) return;//判断图像是否为空，如果对空图像进行操作会出现未知的错误
+
+	int w=m_Image.GetWidth();//获得图像的宽度
+	int h=m_Image.GetHeight();//获得图像的高度
+	for (int j=0;j<h;j++)
+	{
+		for (int k=0;k<w;k++)
+		{
+			m_Image.m_pBits[0][j][k]=0;//B   用循环访问图像的像素值，将它的绿色分量和蓝色分量置为0，图像就只剩下红色分量了
+			m_Image.m_pBits[1][j][k]=0;//G
+
+
+		}
+	}
+
+	Invalidate(1); //强制调用ONDRAW函数，ONDRAW会绘制图像
+}
+
+
+//void CImage_ProcessingView::OnViewToolbar()
+//{
+//	// TODO: 在此添加命令处理程序代码
+//}
